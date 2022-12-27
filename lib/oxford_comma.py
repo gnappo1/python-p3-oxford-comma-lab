060234e05f3d818e56b18286c409047a32b9558d
Matteo Piccini <gnappo.1@live.it>
2022-12-27 13:53:31 -0800

    DOne

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,4 @@
 def oxford_comma(items):
-    return None
+    if len(items) <= 1: return items[0]
+    return ", ".join(items[0:-1]) + (f" and {items[-1]}" if len(items) == 2 else f", and {items[-1]}")
+    # return (", ".join(items[0:-1]) + f" and {items[-1]}") if len(items) == 2 else (", ".join(items[0:-1]) + f", and {items[-1]}")
